ad97346c47a6a32188ef7011ee7e78f1b82c311a
Brian Stiner <Brian.Stiner@revature.net>
2022-05-12 12:42:17 -0700

    added ui kit

--- templates/script.js
+++
@@ -1,8 +0,0 @@
-function LikeButtonClicked(el) {
-    console.log(el);
-    if (el.textContent === 'thumb_up') {
-        el.textContent = 'thumb_down';
-    } else {
-        el.textContent = 'thumb_up';
-    }
-}
