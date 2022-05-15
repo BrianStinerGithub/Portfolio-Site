9c1a85c5941e03ccd017391e78c2f4ee4d60584d
Brian Stiner <Brian.Stiner@revature.net>
2022-05-14 20:39:03 -0700

    adding trailing slashed to routes to fix freeze.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 def portfolio():
     return render_template('portfolio.html', portfolio = projects)
 
-@app.route('/about')
+@app.route('/about/')
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact/')
 def contact():
     return render_template('contact.html')
 
-@app.route('/contact', methods=['POST'])
+@app.route('/contact/', methods=['POST'])
 def send_email():
     return render_template('thankyou.html')
 
