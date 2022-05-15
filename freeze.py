e7d7527d2ae3a836349b11af7ae7b6462ad27c7a
Brian Stiner <Brian.Stiner@revature.net>
2022-05-14 20:16:03 -0700

    animated and content filled about page, freezer

--- freeze.py
+++ freeze.py
@@ -1,5 +1,10 @@
 # yaml file runs this script to create a static website
-import frozen_flask
+import flask_frozen import Freezer
 import app
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
 
 
